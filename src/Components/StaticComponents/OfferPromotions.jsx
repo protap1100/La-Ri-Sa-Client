e3a2e636e987c82e28f28faff243c809f570a03b
protap1100 <protapb23@gmail.com>
2024-05-09 22:44:39 +0600

    offerpromotions added

--- src/Components/StaticComponents/OfferPromotions.jsx
+++ src/Components/StaticComponents/OfferPromotions.jsx
@@ -1,8 +1,19 @@
 const OfferPromotions = () => {
     return (
         <div>
-            <div className="h-40 md:w-full w-full lg:w-4/6 rounded-xl mx-auto bg-gradient-to-r from-blue-500 via-purple-100 to-indigo-500">
-
+            <div className="h-40 md:w-full w-full flex items-center lg:w-4/6 rounded-xl mx-auto bg-gradient-to-r from-blue-500 via-purple-100 to-indigo-500">
+                <div className="flex  bg-red-400 items-center justify-between">
+                    <div className="text-lg">
+                        <p>Exclusive Offer!</p>
+                        <ul>
+                            <li>Up to <strong>25% Discount</strong> on booking LaRiSa Homes from our website</li>
+                            <li> <strong>First 100 Bookings </strong> to receive a bottle of fine indian wine</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className="font-bold bg-black p-3 text-white gap-4">Book Now</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
