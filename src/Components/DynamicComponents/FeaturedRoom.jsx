7d48c0ace563f232919ddaee04a3a1236a083de8
protap1100 <protapb23@gmail.com>
2024-05-14 19:47:04 +0600

    ui upgrade

--- src/Components/DynamicComponents/FeaturedRoom.jsx
+++ src/Components/DynamicComponents/FeaturedRoom.jsx
@@ -24,22 +24,45 @@ const FeaturedRoom = () => {
   return (
     <div className="my-20">
       <div className="my-10 space-y-3">
-          <h1 className="text-center font-bold text-3xl">Here is All Our Featured Room</h1>
-          <h1 className="text-center px-4">These Room are available For booking You can check about peoples review about this these room  by clicking view details</h1>
+        <h1 className="text-center font-bold text-3xl">
+          Here is All Our Featured Room
+        </h1>
+        <h1 className="text-center px-4">
+          These Room are available For booking You can check about peoples
+          review about this these room by clicking view details
+        </h1>
       </div>
       {loading ? (
         <div className="flex justify-center items-center my-40">
           <span className="text-accent loading text-center loading-spinner loading-lg"></span>
         </div>
       ) : (
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
-            {
-                featuredRoom.slice(0,6).map(room => <SingleFeaturedRoom key={room._id} room={room}></SingleFeaturedRoom> )
-            }
+        <div>
+          {featuredRoom.length < 1 ? (
+            <div className="text-center text-2xl">No Rooms Available</div>
+          ) : (
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+              {featuredRoom?.slice(0, 6).map((room) => (
+                <SingleFeaturedRoom
+                  key={room._id}
+                  room={room}
+                ></SingleFeaturedRoom>
+              ))}
+            </div>
+          )}
         </div>
       )}
       <div className="text-center mt-10">
-        <Link to='/allRoom' className="bg-btn p-2 text-white font-bold rounded hover:bg-btn-hover">See All Rooms</Link>
+        {featuredRoom.length < 1 ? (
+          ""
+        ) : (
+          <Link
+            to="/allRoom"
+            className="bg-btn p-2 text-white font-bold rounded hover:bg-btn-hover"
+          >
+            See All Rooms
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/Components/StaticComponents/Footer.jsx
+++ src/Components/StaticComponents/Footer.jsx
@@ -1,66 +1,88 @@
-import { SocialIcon } from 'react-social-icons'
+import { SocialIcon } from "react-social-icons";
+import logo from '../../assets/logo.png'
 
 const Footer = () => {
-    return (
-      <div className="bg-gray-800 text-white py-10 rounded-xl">
-        <footer className="container mx-auto px-4">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
-            <div className="space-y-4">
-              <h2 className="font-medium">Explore Rooms</h2>
-              <div className="flex flex-col space-y-2 text-sm">
-                <a href="#">View Rooms</a>
-                <a href="#">Room Types</a>
-                <a href="#">Special Offers</a>
-                <a href="#">Book Now</a>
-              </div>
-            </div>
-  
-            <div className="space-y-4">
-              <h2 className="font-medium">Discover Destinations</h2>
-              <div className="flex flex-col space-y-2 text-sm">
-                <a href="#">Popular Destinations</a>
-                <a href="#">Local Attractions</a>
-                <a href="#">Guided Tours</a>
-                <a href="#">Book Activities</a>
-              </div>
-            </div>
-  
-            <div className="space-y-4">
-              <h2 className="font-medium">About Us</h2>
-              <div className="flex flex-col space-y-2 text-sm">
-                <a href="#">Our Story</a>
-                <a href="#">Team Members</a>
-                <a href="#">Events & News</a>
-                <a href="#">Customer Reviews</a>
-              </div>
-            </div>
-  
-            <div className="space-y-4">
-              <h2 className="font-medium">Connect With Us</h2>
-              <div className="flex flex-col space-y-2 text-sm">
-                <a href="#">Contact Us</a>
-                <a href="#">FAQs</a>
-                <a href="#">Subscribe to Newsletter</a>
-                <a href="#">Follow Us on Social Media</a>
-              </div>
+  return (
+    <div className="bg-gray-800 text-white py-5 rounded-xl">
+      <footer className="container mx-auto px-4">
+        <div className="flex justify-center gap-10 my-3 items-center">
+          <h1>About La Ri Sa</h1>
+          <img className="h-10 w-20 rounded-xl" src={logo} alt="" />
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+          <div className="space-y-4 text-center">
+            <h2 className="font-medium">Explore Rooms</h2>
+            <div className="flex flex-col space-y-2 text-sm">
+              <a href="#">View Rooms</a>
+              <a href="#">Room Types</a>
+              <a href="#">Special Offers</a>
+              <a href="#">Book Now</a>
             </div>
           </div>
 
-          <div className="rounded-xl text-center p-9">
-                <div className="flex justify-center mt-5 gap-5">  
-                    <h1 href="#"><i className="text-2xl"><SocialIcon url="https://facebook.com" /> </i></h1>
-                    <h1 href="#"><i className="text-2xl"><SocialIcon url="https://instagram.com" /></i></h1>
-                    <h1 href="#"><i className="text-2xl"><SocialIcon url="https://twitter.com" /></i></h1>
-                    <h1 href="#"><i className="text-2xl"><SocialIcon url="https://github.com" /></i></h1>
-                </div>
+          <div className="space-y-4  text-center">
+            <h2 className="font-medium">Discover Destinations</h2>
+            <div className="flex flex-col space-y-2 text-sm">
+              <a href="#">Popular Destinations</a>
+              <a href="#">Local Attractions</a>
+              <a href="#">Guided Tours</a>
+              <a href="#">Book Activities</a>
             </div>
-          <div className="flex items-center justify-center  text-sm">
-            <span>&copy; {new Date().getFullYear()} Wanderlust Booking. All Rights Reserved.</span>
           </div>
-        </footer>
-      </div>
-    );
-  };
-  
-  export default Footer;
-  
+          <div className="space-y-4  text-center">
+            <h2 className="font-medium">About Us</h2>
+            <div className="flex flex-col space-y-2 text-sm">
+              <a href="#">Our Story</a>
+              <a href="#">Team Members</a>
+              <a href="#">Events & News</a>
+              <a href="#">Customer Reviews</a>
+            </div>
+          </div>
+
+          <div className="space-y-4  text-center">
+            <h2 className="font-medium">Connect With Us</h2>
+            <div className="flex flex-col space-y-2 text-sm">
+              <a href="#">Contact Us</a>
+              <a href="#">FAQs</a>
+              <a href="#">Subscribe to Newsletter</a>
+              <a href="#">Follow Us on Social Media</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="rounded-xl text-center p-9">
+          <div className="flex justify-center mt-5 gap-5">
+            <h1 href="#">
+              <i className="text-2xl">
+                <SocialIcon url="https://facebook.com" />{" "}
+              </i>
+            </h1>
+            <h1 href="#">
+              <i className="text-2xl">
+                <SocialIcon url="https://instagram.com" />
+              </i>
+            </h1>
+            <h1 href="#">
+              <i className="text-2xl">
+                <SocialIcon url="https://twitter.com" />
+              </i>
+            </h1>
+            <h1 href="#">
+              <i className="text-2xl">
+                <SocialIcon url="https://github.com" />
+              </i>
+            </h1>
+          </div>
+        </div>
+        <div className="flex items-center justify-center  text-sm">
+          <span>
+            &copy; {new Date().getFullYear()} Wanderlust Booking. All Rights
+            Reserved.
+          </span>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
